4882d1f67cb4291ce286c6fffc6b7ab2ef5d82ea
MohamedWael8 <Mohamed.Wael@itworx.com>
2019-03-21 21:45:10 +0200

    Updates

---
+++ precache-manifest.c0dc99c00947f444f9595727b00f089e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-listing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2e241147955cbe93c6a",
+    "url": "/news-listing/static/js/runtime~main.2873d404.js"
+  },
+  {
+    "revision": "a32cececc4337b04e488",
+    "url": "/news-listing/static/js/main.69f76b03.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/news-listing/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "a32cececc4337b04e488",
+    "url": "/news-listing/static/css/main.dfcdfcc4.chunk.css"
+  },
+  {
+    "revision": "fc5ba8771a96ad6db5200915fb47dc76",
+    "url": "/news-listing/index.html"
+  }
+];
